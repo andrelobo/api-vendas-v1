aa529f10ebc12d7556ff713ce4ed2319031baa2d
André Lobo <loboandre@hotmail.com>
2022-10-18 08:57:50 -0400

    feat: add UsersController, usersRoutes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
+import usersRouter from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+
+routes.use('/users', usersRouter);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello Dev !' });
